497e9cdc02fe543b356aecc3aaae195c9d487c95
janithBimsaraUlukkulama <jbimsaraulukkulama@outlook.com>
2021-12-06 00:37:09 +0530

    Handeling fetch error

--- src/Home.js
+++ src/Home.js
@@ -4,20 +4,29 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
             .then(res => {
+                if (!res.ok) {
+                    throw Error('Could not fetchthe data for that resource');
+                }
                 return res.json();
             })
             .then(data => {
                 setBlogs(data);
                 setIsLoading(false);
+                setError(null);
+            }).catch(err => {
+                setIsLoading(false);
+                setError(err.message);
             });
     }, []);
 
     return (
         <div className="home">
+            {error && <div>{error}</div>}
             {isLoading && <div>Loadding...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
