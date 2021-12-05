11f2dc681f68f8cc53706f27c8ee32b1236b461f
janithBimsaraUlukkulama <jbimsaraulukkulama@outlook.com>
2021-12-06 00:55:28 +0530

    Making a Custom Hook

---
+++ src/useFetch.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+// In resct custome hooks need to start with'use'
+const useFetch = (url) => {
+
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch(url)
+            .then(res => {
+                if (!res.ok) {
+                    throw Error('Could not fetchthe data for that resource');
+                }
+                return res.json();
+
+            })
+            .then(data => {
+                setData(data);
+                setIsLoading(false);
+                setError(null);
+
+            }).catch(err => {
+                setIsLoading(false);
+                setError(err.message);
+                
+            });
+    }, []);
+
+    return { data, isLoading, error };
+
+};
+
+export default useFetch;
