37c9aa1b980085c04c3424b6bb4137974c5bab7f
janithBimsaraUlukkulama <jbimsaraulukkulama@outlook.com>
2021-12-07 17:16:13 +0530

    404 Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './Navbar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import  NotFound  from './NotFound';
 
 function App() {
   return (
@@ -23,6 +24,10 @@ function App() {
 
             <Route path="/blogs/:id">
               <BlogDetails />
+            </Route>
+
+            <Route path="*">
+              <NotFound />
             </Route>
 
           </Switch>
